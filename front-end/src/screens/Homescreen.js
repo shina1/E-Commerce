8eebb08876cbfd680778138f4499c31227471ab3
shina1 <oswasiu@gmail.com>
2021-06-30 11:07:47 +0200

    imported data from backend

--- front-end/src/screens/Homescreen.js
+++ front-end/src/screens/Homescreen.js
@@ -1,9 +1,18 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import { Col, Row } from "react-bootstrap";
-import products from "../products";
+// import products from "../products";
 import Products from "../components/Product";
 
 const Homescreen = () => {
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const res = await axios.get("/api/products");
+      console.log(res);
+    };
+    fetchProducts();
+  });
   return (
     <>
       <h1 className="text-center my-3">Latest Products</h1>
